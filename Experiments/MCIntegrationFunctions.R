b55d08cedecf260344558579e51fd500ec6648a0
Marcus Stolz <marcus.stolz@gmail.com>
2023-11-09 18:19:32 +0100

    Buggfixat, samt genomfört ett första experiment på en bivariat nf vid ökande korrelation mellan x och y.
    Det verkas som att variansen av skattningarna ökar vid ökande korrelation särskilt för quasi, relativt sett förstås.

--- Experiments/MCIntegrationFunctions.R
+++ Experiments/MCIntegrationFunctions.R
@@ -3,10 +3,11 @@ library(qrng)
 
 #n-dimensional MC-integration, types of rng can be selected
 mcIntNDim <- function(nvalues, nDim, fDim, vectorLowerLimit, vectorUpperLimit, 
-                      RNG = c("Quasi", "Pseudo"), returnSequentialVector = FALSE) {
+                      RNG = c("Quasi", "Pseudo"), returnSequentialVector = FALSE,
+                      covMatrix = NULL, muVector = NULL) {
   
   randomVariables <- matrix(0, nvalues, nDim)
-  
+
   if (RNG == "Quasi"){
     sobolMatrix <- sobol(nvalues, nDim)
     for (j in 1:nDim) {
@@ -22,7 +23,9 @@ mcIntNDim <- function(nvalues, nDim, fDim, vectorLowerLimit, vectorUpperLimit,
   sumFn <- 0
   sequentialEstimate <-c() # needed for compareMCIntegration graph function
   for(i in 1:nvalues){
-    sumFn <- sumFn + do.call(fDim, as.list(randomVariables[i, ]))
+    #sumFn <- sumFn + do.call(fDim, as.list(randomVariables[i, ],covMatrix = covMatrix)) #knas borde ändra fdim så att den tar emot en vector
+    
+    sumFn <- sumFn + fDim(randomVariables[i,], covMatrix = covMatrix, muVector = muVector, nDim = nDim)
     estimate <- prod(vectorUpperLimit - vectorLowerLimit) * sumFn/i
     sequentialEstimate <- append(sequentialEstimate, estimate) 
     
@@ -45,25 +48,30 @@ mcIntNDim <- function(nvalues, nDim, fDim, vectorLowerLimit, vectorUpperLimit,
 
 # Creates a scatterplot of the convergense of the MC Integration
 compareMcIntegration <- function(nseries, nvalues, nDim, fDim, vectorLowerLimit, 
-                                 vectorUpperLimit, vectorRNG, main = "Comparison of Series"){
-  
+                                 vectorUpperLimit, vectorRNG, main = "Comparison of Series",
+                                 covMatrix = NULL, muVector = NULL){
+ 
   matrixOfSeries <- matrix(0,nvalues, nseries)
   
   for (i in 1:nseries){
-    matrixOfSeries[,i] <- mcIntNDim(nvalues, nDim, fDim, vectorLowerLimit, vectorUpperLimit, RNG = vectorRNG[i], returnSequentialVector = TRUE)
+    matrixOfSeries[,i] <- mcIntNDim(nvalues, nDim, fDim, vectorLowerLimit, vectorUpperLimit, RNG = vectorRNG[i], 
+                                    returnSequentialVector = TRUE, covMatrix = covMatrix, muVector = muVector) #length 0??
   }
   
   seriesColor <- c("red","blue", "green", "orange", "purple", "yellow")
   
   #print estimates
   for(i in 1:nseries){
-    print(paste(vectorRNG[i],"-Estimate:",matrixOfSeries[nvalues,i])    )
+    Estimate <- matrixOfSeries[nvalues,i]
+    varianceOfEstimate <- var(matrixOfSeries[,i])
+    print(paste(vectorRNG[i],"-Estimate:", Estimate))
+    print(paste("Variance of ",vectorRNG[i],"-Estimate:", varianceOfEstimate))
   }
   
   
   #scatterplot
   plot(1:nvalues,matrixOfSeries[,1] ,cex = 0.2, pch = 20, col=seriesColor[1], 
-    ylim = c(matrixOfSeries[nvalues, 1] -0.005, matrixOfSeries[nvalues,1] +0.005),
+    #ylim = c(matrixOfSeries[nvalues, 1] -0.005, matrixOfSeries[nvalues,1] +0.005),
     main = main , ylab = "Estimate", xlab = "n")
   for (i in 2:nseries){
     points(matrixOfSeries[,i],cex = 0.2, pch = 20, col = seriesColor[i])
@@ -73,8 +81,8 @@ compareMcIntegration <- function(nseries, nvalues, nDim, fDim, vectorLowerLimit,
   
   #Histogram of estimate
   for(i in 1:nseries){
-    hist(matrixOfSeries[,i], main = vectorRng[i], breaks = round(nvalues/2),
-         xlim = c(matrixOfSeries[nvalues,1] -0.01, 
-         matrixOfSeries[nvalues,1] +0.01), xlab = "Estimate")
+    hist(matrixOfSeries[,i], main = paste("Distribution of ",vectorRng[i],"-Estimate"), breaks = round(nvalues/2),
+         #xlim = c(matrixOfSeries[nvalues,1] -0.01, matrixOfSeries[nvalues,1] +0.01), 
+         xlab = "Estimate")
   }
 }
