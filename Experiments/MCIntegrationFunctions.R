6d935501ce594908942cfa187315c910a18316a5
Mrcsstlz <marcus.stolz@gmail.com>
2023-11-13 18:43:00 +0100

    La till ett argument start i mc-funktionerna, där man kan välja att slänga bort de första observationerna. Burnoff tror jag det heter

--- Experiments/MCIntegrationFunctions.R
+++ Experiments/MCIntegrationFunctions.R
@@ -4,7 +4,7 @@ library(qrng)
 #n-dimensional MC-integration, types of rng can be selected
 mcIntNDim <- function(nValues, nDim, fDim, vectorLowerLimit, vectorUpperLimit, 
                       RNG = c("Quasi", "Pseudo"), returnSequentialVector = FALSE,
-                      covMatrix = NULL, muVector = NULL, df = NULL) {
+                      covMatrix = NULL, muVector = NULL, df = NULL, start = 1) {
   
   randomVariables <- matrix(0, nValues, nDim)
 
@@ -20,19 +20,21 @@ mcIntNDim <- function(nValues, nDim, fDim, vectorLowerLimit, vectorUpperLimit,
       randomVariables[, j] <- runif(nValues, vectorLowerLimit[j], vectorUpperLimit[j])
     }
   }
+  
   sumFn <- 0
   sequentialEstimate <-c() # needed for compareMCIntegration graph function
-  for(i in 1:nValues){
-    #sumFn <- sumFn + do.call(fDim, as.list(randomVariables[i, ],covMatrix = covMatrix)) 
-    
+  
+  
+  for(i in start:nValues){
     sumFn <- sumFn + fDim(randomVariables[i,], covMatrix = covMatrix, muVector = muVector, nDim = nDim)#, df = df)
     estimate <- prod(vectorUpperLimit - vectorLowerLimit) * sumFn/i
     sequentialEstimate <- append(sequentialEstimate, estimate) 
     
   }
   
- 
-  estimate <- sequentialEstimate[nValues] 
+print(RNG)
+print(tail(sequentialEstimate,10))
+  estimate <- sequentialEstimate[nValues-start+1] 
   
   #If sequentialVector is chosen a vector of estimates is returned, otherwise the final estimate.
   if(!returnSequentialVector){
@@ -49,43 +51,43 @@ mcIntNDim <- function(nValues, nDim, fDim, vectorLowerLimit, vectorUpperLimit,
 # Creates a scatterplot of the convergense of the MC Integration
 compareMcIntegrationGraph <- function(nSeries, nValues, nDim, fDim, vectorLowerLimit, 
                                  vectorUpperLimit, vectorRNG, main = "Comparison of Series",
-                                 covMatrix = NULL, muVector = NULL, df = NULL){
- 
-  matrixOfSeries <- matrix(0, nValues, nSeries)
+                                 covMatrix = NULL, muVector = NULL, df = NULL, start = 1){
+  
+  matrixOfSeries <- matrix(0, nValues-start+1, nSeries)
   
   for (i in 1:nSeries){
     matrixOfSeries[,i] <- mcIntNDim(nValues, nDim, fDim, vectorLowerLimit, vectorUpperLimit, RNG = vectorRNG[i], 
-                                    returnSequentialVector = TRUE, covMatrix = covMatrix, muVector = muVector) 
+                                    returnSequentialVector = TRUE, covMatrix = covMatrix, muVector = muVector, start = start) 
   }
   
+
   seriesColor <- c("red","blue", "green", "orange", "purple", "yellow")
-  
-  
-    #print estimates
-    for(i in 1:nSeries){
-      Estimate <- matrixOfSeries[nValues,i]
-      varianceOfEstimate <- var(matrixOfSeries[,i])
-      print(paste(vectorRNG[i],"-Estimate:", Estimate))
-      print(paste("Variance of ",vectorRNG[i],"-Estimate:", varianceOfEstimate))
-    }
+    
+  #print estimates
+  for(i in 1:nSeries){
+    Estimate <- matrixOfSeries[nValues-start+1,i]
+    varianceOfEstimate <- var(matrixOfSeries[,i])
+    print(paste(vectorRNG[i],"-Estimate:", Estimate))
+    print(paste("Variance of ",vectorRNG[i],"-Estimate:", varianceOfEstimate))
+  }
   
   
     #scatterplot
-    plot(1:nValues,matrixOfSeries[,1] ,cex = 0.2, pch = 20, col=seriesColor[1], 
-      #ylim = c(matrixOfSeries[nValues, 1] -0.005, matrixOfSeries[nValues,1] +0.005),
-      main = main , ylab = "Estimate", xlab = "n")
-    for (i in 2:nSeries){
-      points(matrixOfSeries[,i],cex = 0.2, pch = 20, col = seriesColor[i])
-    }
-      legend("topright", legend = vectorRNG, 
-      col = seriesColor[1:nSeries], pch = 20, cex = 0.8)
+  plot(1:(nValues-start+1),matrixOfSeries[,1] ,cex = 0.2, pch = 20, col=seriesColor[1], 
+    #ylim = c(matrixOfSeries[nValues, 1] -0.005, matrixOfSeries[nValues,1] +0.005),
+    main = main , ylab = "Estimate", xlab = "n")
+  for (i in 2:nSeries){
+    points(matrixOfSeries[,i],cex = 0.2, pch = 20, col = seriesColor[i])
+  }
+    legend("topright", legend = vectorRNG, 
+    col = seriesColor[1:nSeries], pch = 20, cex = 0.8)
   
-    #Histogram of estimate
-    for(i in 1:nSeries){
-      hist(matrixOfSeries[,i], main = paste("Distribution of ",vectorRng[i],"-Estimate"), breaks = round(nValues/2),
-         #xlim = c(matrixOfSeries[nValues,1] -0.01, matrixOfSeries[nValues,1] +0.01), 
-         xlab = "Estimate")
-    }
+  #Histogram of estimate
+  for(i in 1:nSeries){
+    hist(matrixOfSeries[,i], main = paste("Distribution of ",vectorRng[i],"-Estimate"), breaks = round(nValues/2),
+      #xlim = c(matrixOfSeries[nValues,1] -0.01, matrixOfSeries[nValues,1] +0.01), 
+      xlab = "Estimate")
+  }
   
   
 }
@@ -94,21 +96,23 @@ compareMcIntegrationGraph <- function(nSeries, nValues, nDim, fDim, vectorLowerL
 #returns matrix of metrics of estimate
 compareMcIntegrationMetrics <- function(nSeries, nValues, nDim, fDim, vectorLowerLimit, 
                                       vectorUpperLimit, vectorRNG, covMatrix = NULL, 
-                                      muVector = NULL, df = NULL){
+                                      muVector = NULL, df = NULL, start = 1){
   
-  matrixOfSeries <- matrix(0, nValues, nSeries)
   
+  matrixOfSeries <- matrix(0, nValues-start+1, nSeries)
+  
+ 
   for (i in 1:nSeries){
     matrixOfSeries[,i] <- mcIntNDim(nValues, nDim, fDim, vectorLowerLimit, vectorUpperLimit, RNG = vectorRNG[i], 
-                                    returnSequentialVector = TRUE, covMatrix = covMatrix, muVector = muVector) 
+                                    returnSequentialVector = TRUE, covMatrix = covMatrix, muVector = muVector, start = start) 
   }
   
   matrixOFEstimateMetrics <- matrix(0, nrow = 3, ncol = nSeries)
   
   for(i in 1:nSeries){
-    matrixOFEstimateMetrics[1, i] <- matrixOfSeries[nValues, i]  # Estimate
+    matrixOFEstimateMetrics[1, i] <- matrixOfSeries[nValues-start+1, i]  # Estimate
     matrixOFEstimateMetrics[2, i] <- var(matrixOfSeries[, i])    # Variance
-    matrixOFEstimateMetrics[3, i] <- sum((matrixOfSeries[, i] - matrixOfSeries[nValues, i])^2) / nValues  # MSE
+    matrixOFEstimateMetrics[3, i] <- sum((matrixOfSeries[, i] - matrixOfSeries[nValues-start+1, i])^2) / (nValues-start+1)  # MSE
   }
   rownames(matrixOFEstimateMetrics) <- c("Estimate", "Variance", "MSE")
   colnames(matrixOFEstimateMetrics) <- vectorRNG#paste("Series", seq_len(nSeries))
